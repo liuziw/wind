9412621084d9eef68a85989e4611f6eebf7396f7
liuzifjxc <719050078@qq.com>
2019-07-16 15:58:38 +0800

    eureka

---
+++ eureka-server/src/main/java/com/lzw/wind/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.lzw.wind.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author liys
+ * @date 2018/6/148:58
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
