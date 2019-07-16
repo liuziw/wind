62446ab00429da60df3ca6e6de194a1b029a5768
liuzifjxc <719050078@qq.com>
2019-07-16 11:35:18 +0800

    生成代码

---
+++ test/src/main/java/com/lzw/wind/test/daomanager/DeptDaoManager.java
@@ -0,0 +1,8 @@
+package com.lzw.wind.test.daomanager;
+
+import com.lzw.wind.test.daomanager.immutable.ImmutableDeptDaoManager;
+
+
+public interface DeptDaoManager extends  ImmutableDeptDaoManager{
+
+}
---
+++ test/src/main/java/com/lzw/wind/test/daomanager/UserDaoManager.java
@@ -0,0 +1,8 @@
+package com.lzw.wind.test.daomanager;
+
+import com.lzw.wind.test.daomanager.immutable.ImmutableUserDaoManager;
+
+
+public interface UserDaoManager extends  ImmutableUserDaoManager{
+
+}
