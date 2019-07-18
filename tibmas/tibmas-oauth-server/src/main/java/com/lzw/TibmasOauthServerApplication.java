244f21c95ead566c4e2f9a0fb8faaf9eacb9eb64
liuzifjxc <719050078@qq.com>
2019-07-18 15:25:17 +0800

    优化

--- tibmas/tibmas-oauth-server/src/main/java/com/lzw/TibmasOauthServerApplication.java
+++ tibmas/tibmas-oauth-server/src/main/java/com/lzw/TibmasOauthServerApplication.java
@@ -1,4 +1,4 @@
-package com.lzw.wind.tibmas;
+package com.lzw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- tibmas/tibmas-webapi/src/main/java/com/lzw/TibmasWebApiApplication.java
+++ tibmas/tibmas-webapi/src/main/java/com/lzw/TibmasWebApiApplication.java
@@ -1,4 +1,4 @@
-package com.lzw.wind.tibmas.webapi;
+package com.lzw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
